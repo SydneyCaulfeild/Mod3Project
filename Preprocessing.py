19477a3848b9fe5cc980cecb1f4f521698d62d12
18sue <47460889+18sue@users.noreply.github.com>
2019-03-10 17:48:57 -0400

    image open and resize 1

--- Preprocessing.py
+++ Preprocessing.py
@@ -1,1 +1,12 @@
-print ("hello world")
+from PIL import Image
+im = Image.open(pathlib.path("image title"))
+ # opening image , https://pillow.readthedocs.io/en/stable/reference/open_files.html#file-handling
+im = Image.crop(box=L, uL, R, lL)
+# crop image, coordinate system L= bottom left corner, uL = upper left, R = upper Right...
+
+from PIL import ImageFilter
+im = im.filter(ImageFilter. )
+#runs image thru filter, edge enhance/ edge enhance more/ find edges
+
+im = PIL.ImageOps.grayscale("image")
+# greyscale the image
