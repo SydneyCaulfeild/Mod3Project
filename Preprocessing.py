6c23007c74425f772e67f824a3af2f2803476635
18sue <47460889+18sue@users.noreply.github.com>
2019-03-14 15:22:02 -0400

    Preprocessing with resizing and edge enhance

--- Preprocessing.py
+++ Preprocessing.py
@@ -1,12 +1,35 @@
 from PIL import Image
-im = Image.open(pathlib.path("image title"))
- # opening image , https://pillow.readthedocs.io/en/stable/reference/open_files.html#file-handling
-im = Image.crop(box=L, uL, R, lL)
+from PIL import ImageFilter
+import pathlib
+import time
+from datetime import datetime
+
+########################################
+#def current_time():
+    #return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+#print current_time()
+#########################################
+
+im = Image.open(r"C:\Users\susu1\Documents\Image processing\hurricane.png")
+ # opening image
+
+if im is None:
+    print ('no pic')
+
+print (im)
+
+#im = im.crop((0,0,1024,1024))
 # crop image, coordinate system L= bottom left corner, uL = upper left, R = upper Right...
 
-from PIL import ImageFilter
-im = im.filter(ImageFilter. )
+im = im.resize((910, 512))
+# size is w x h, resizing filter - if none, PIL.Image.NEAREST, box- not needed
+
+im = im.filter(ImageFilter.EDGE_ENHANCE)
 #runs image thru filter, edge enhance/ edge enhance more/ find edges
 
-im = PIL.ImageOps.grayscale("image")
+##im = ImageOps.grayscale("hurricane.png")
 # greyscale the image
+
+print (im)
+
+im.save("cropped_hurricane.png")
