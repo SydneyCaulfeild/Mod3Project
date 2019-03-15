aa8193c854d960007d9e612ae57a209fc6465832
18sue <47460889+18sue@users.noreply.github.com>
2019-03-15 15:51:11 -0400

    added geolocation and datetime

--- Preprocessing.py
+++ Preprocessing.py
@@ -3,11 +3,35 @@ from PIL import ImageFilter
 import pathlib
 import time
 from datetime import datetime
+import requests
+import json
 
-########################################
-#def current_time():
-    #return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-#print current_time()
+send_url = "http://api.ipstack.com/check?access_key=ae5620e07d25c95d8baca860f7fdc0d6"
+geo_req = requests.get(send_url)
+geo_json = json.loads(geo_req.text)
+latitude = geo_json['latitude']
+longitude = geo_json['longitude']
+city = geo_json['city']
+print(latitude, longitude, city)
+
+## test lat
+if latitude is None:
+    print('no lat')
+##test long
+if longitude is None:
+    print ('no long')
+##test city
+if city is None:
+    print ('no city')
+
+##########################################
+def current_time():
+    return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+print (current_time())
+
+## test
+if current_time() is None:
+    print('no datetime')
 #########################################
 
 im = Image.open(r"C:\Users\susu1\Documents\Image processing\hurricane.png")
@@ -27,8 +51,8 @@ im = im.resize((910, 512))
 im = im.filter(ImageFilter.EDGE_ENHANCE)
 #runs image thru filter, edge enhance/ edge enhance more/ find edges
 
-##im = ImageOps.grayscale("hurricane.png")
-# greyscale the image
+#im = ImageOps.grayscale("hurricane.png")
+## greyscale the image
 
 print (im)
 
