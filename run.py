f2ce4d49bf6df178176b3a06c92170701438d4b4
nignogthebignog <47460677+nignogthebignog@users.noreply.github.com>
2019-03-22 13:26:05 -0400

    Update run.py
    runs on folder, outputs %hurricanes

--- run.py
+++ run.py
@@ -4,15 +4,48 @@ Created on Tue Mar 12 20:13:45 2019
 
 @author: bzbri
 """
-import cv2
+from PIL import Image
+from PIL import ImageFilter
 from keras.models import load_model
 
-model = load_model("model.h5")
-image = cv2.imread("cat_or_dog_1.jpg",1)
-print(image.shape)
-image = cv2.resize(image,(64,64))
-output = model.predict(image.reshape(-1,64,64,3))
+import os
+import cv2
+import glob
 
-print(output)
+i = 0
+j = 0
 
-#model.summary()
+yes = 0
+no = 0
+
+im_dir = r"dataset/train/Nothing"
+data_path = os.path.join(im_dir,'*g')
+files = glob.glob(data_path)
+
+model = load_model("model512PRE.h5")
+
+for f in files:
+    i += 1
+    im = Image.open(f)
+    im = im.filter(ImageFilter.EDGE_ENHANCE)
+    #im = ImageOps.autocontrast(im, cutoff=0, ignore=None)
+    
+    im.save("temp"+str(i)+".png")
+
+for n in range(i):
+    j += 1
+    img = cv2.imread("temp"+str(j)+".png",1)
+    img = cv2.resize(img,(64,64))
+    output = model.predict(img.reshape(-1,64,64,3))
+
+    if output[0][0]==1:
+        yes+=1
+    
+    elif output[0][1]==1:
+        no+=1
+        
+    os.remove("temp"+str(j)+".png")
+
+print("Hurricanes:",yes)
+print("Total Images:",yes+no)
+print("Percentage of Hurricanes:",100*yes/(yes+no),"%")
